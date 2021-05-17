276bb980210138b12fe8668aaf772c6f70c8713e
Andre Vallestero <andrevallestero@gmail.com>
2021-05-17 14:45:00 -0400

    Refactored typescript

--- index.js
+++
@@ -1,5 +0,0 @@
-const koi_tools = require("./src/tool");
-const koi_node = require("./src/node");
-const bundler_node = require("./src/bundler_node");
-
-module.exports = { koi_tools, koi_node, bundler_node };
